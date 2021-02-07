f40eec5fe80d6b1b60f400ee6449d735deb5b714
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2021-02-07 14:59:43 -0800

    Added express.static middleware

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static("public"));
+
 app.get("/", function(req, res) {
-    res.send("Hello world");
+    res.send("Hello world!");
 });
 
 app.listen(PORT, function() {
